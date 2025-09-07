920dc47625be703f14ae70a15bf2140ff8bb445e
ugpodvelezje <ugpodvelezje.dev@gmail.com>
2025-09-07 17:27:07 +0200

    Update Title Tags & Meta Descriptions and better keyword targeting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,32 +9,32 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: 'Početna | UG Podveležje'
+    title: 'Podveležje - Čuvari tradicije i prirode planine Velež'
   },
   {
     path: 'about',
     component: AboutComponent,
-    title: 'O nama | UG Podveležje'
+    title: 'O nama - Udruženje građana Podveležje | Kulturna baština'
   },
   {
     path: 'news',
     loadChildren: () => import('./news/news.module').then(m => m.NewsModule),
-    title: 'Novosti | UG Podveležje'
+    title: 'Novosti iz Podveležja | UG Podveležje'
   },
   {
     path: 'contact',
     component: ContactComponent,
-    title: 'Kontakt | UG Podveležje'
+    title: 'Kontakt - Udruženje građana Podveležje'
   },
   {
     path: 'membership',
     component: MembershipPricingComponent,
-    title: 'Članstvo | UG Podveležje'
+    title: 'Članstvo u udruženju Podveležje | UG Podveležje'
   },
   {
     path: 'heroes',
     component: HeroesComponent,
-    title: 'Heroji | UG Podveležje'
+    title: 'Heroji Podveležja | Kulturna baština'
   },
   {
     path: '**',
